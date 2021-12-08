e4abc7c2bd78495a886b416a7896bde550e79cb4
YuraBobeshko <gorobech55@gmail.com>
2021-12-08 16:00:16 +0200

    'sol'

--- src/pages/structure/Structure.tsx
+++ src/pages/structure/Structure.tsx
@@ -1,35 +1,28 @@
 import React, {useCallback, useEffect, useMemo} from "react";
 import {useSelector, useDispatch} from "react-redux";
-import {useParams} from "react-router-dom";
-
 import {TreeRenerer, Downloader, Uploader} from "../../components";
 import {IGetStructure, IProject} from "../../types";
 import {defaultTree} from "../../constants";
-import {updateListProject} from "../../redux/thunks";
+import {setСurrentProject} from "../../redux/thunks/currentProject";
 
 function Structure() {
-    const {currentId} = useParams<{ currentId: string | undefined }>();
     const dispatch = useDispatch();
-    const project = useSelector((state) =>
-        state.listProject.find((project) => project.id === currentId)
-    ) as IProject;
+    const project = useSelector((state) => state.currentProject) as IProject;
 
     const tree = useMemo(
-        () =>
-            project?.structure || []
-        ,
+        () => project?.structure || [],
         [project?.structure],
     );
+
     const setTree = useCallback(
-        (structure: IGetStructure) => {
-            updateListProject(dispatch,{...project, structure: structure(tree)}).then()
-        }
-        ,
-        [dispatch, project, tree],
+        (callBack: IGetStructure) => {
+            setСurrentProject(dispatch, callBack(tree)).then()
+        },
+        [dispatch, tree],
     );
 
     useEffect(() => {
-        setTree(() => defaultTree);
+      if(project) setTree(() => defaultTree);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
